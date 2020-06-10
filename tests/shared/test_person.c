491a8ad62a863dfa795f2130c44432616e84a8a4
IVORY_C <767478058@qq.com>
2020-06-10 23:43:17 +0800

    测试Person结构体能否正常使用

---
+++ tests/shared/test_person.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <assert.h>
+
+#include "..\..\domain\shared\person.c"
+
+int main(){
+    struct Person person_one = {"Lisa", 12, "female"};
+
+    assert(person_one.name == "Lisa");
+    assert(person_one.age == 12);
+    assert(person_one.gender == "female");
+}
