e1332f6e899cbab42ddfc9117588710fdeed1b8a
IVORY_C <767478058@qq.com>
2020-06-18 16:20:29 +0800

    创建values.h

---
+++ domain/values.h
@@ -0,0 +1,13 @@
+#ifndef STRUCTURES_H
+#define STRUCTURES_H
+
+#define MAX 100
+#define SUCCESS 1
+#define FAILURE 0
+#define INVALID_AGE -1
+#define INVALID_GENDER -2
+#define INVALID_START -3
+#define INVALID_STEP -4
+#define INVALID_NUMBER -5
+
+#endif
